f33aa65cafd505e4e48af08072eb151dc51176be
salmonfishycooked <samonfishycooked@gmail.com>
2023-04-17 10:08:12 +0800

    modify Datastructure LinkedStack

--- src/com/datastructure/LinkedStack.java
+++ src/com/datastructure/LinkedStack.java
@@ -4,10 +4,11 @@ import java.util.NoSuchElementException;
 
 /**
  * 栈（链式存储）
- * @param <T> 栈所存放的元素类型
+ * 栈是一种特殊的线性表，是一种满足 LIFO(Last In, First Out) 特性的数据结构
+ * @param <T> 所存放的元素类型
  */
 public class LinkedStack<T> {
-    private final Node<T> head = new Node<>(null); // 头结点
+    private final Node<T> head = new Node<>(null); // 头结点，其指向栈顶
 
     /**
      * 结点类
@@ -36,10 +37,17 @@ public class LinkedStack<T> {
      * @return 被出栈的元素
      */
     public T pop() {
-        checkEmpty();
-        T element = head.next.element;
+        T element = peek();
         head.next = head.next.next;
         return element;
+    }
+
+    /**
+     * 获取栈顶元素
+     */
+    public T peek() {
+        checkEmpty();
+        return head.next.element;
     }
 
     public boolean isEmpty() {
