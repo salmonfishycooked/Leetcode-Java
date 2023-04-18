e84d7d3286c0e24584693d139e38d4abc76c0bc1
salmonfishycooked <samonfishycooked@gmail.com>
2023-04-18 12:05:36 +0800

    modify Leetcode 162

--- src/com/leetcode/_162_Find_Peak_Element/Solution.java
+++ src/com/leetcode/_162_Find_Peak_Element/Solution.java
@@ -1,5 +1,10 @@
 package com.leetcode._162_Find_Peak_Element;
 
+/**
+ * 二分查找
+ * 时间复杂度：O(logn)
+ * 空间复杂度：O(1)
+ */
 class Solution {
     public int findPeakElement(int[] nums) {
         int l = -1, r = nums.length - 1;
